6be56e72eeaccd24a27f119bef6324bd52adf859
Patrick Kraaij <patrick.kraaij@gmail.com>
2023-06-28 14:23:56 +0200

    feat: Adding try catch block for GET requests

--- src/games/games.service.ts
+++ src/games/games.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Game } from './games.model';
@@ -10,12 +10,20 @@ export class GamesService {
   constructor(@InjectModel('Game') private readonly gameModel: Model<Game>) {}
 
   async getAllGames(): Promise<Game[]> {
-    this.games = await this.gameModel.find().exec();
-    return this.games;
+    try {
+      this.games = await this.gameModel.find().exec();
+      return this.games;
+    } catch (error) {
+      throw new HttpException(error, 400);
+    }
   }
 
   async getGame(id: string): Promise<Game> {
-    const game = await this.gameModel.findById(id).exec();
-    return game;
+    try {
+      const game = await this.gameModel.findById(id).exec();
+      return game;
+    } catch (error) {
+      throw new HttpException(error, 400);
+    }
   }
 }
